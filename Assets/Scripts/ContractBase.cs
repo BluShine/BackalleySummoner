6c09df06dafc88ad2d713ab78a0379cdf839bf67
Mark <umbreon27@gmail.com>
2016-01-30 01:58:57 -0500

    Contract generation
    Generates contracts.

--- Assets/Scripts/ContractBase.cs
+++ Assets/Scripts/ContractBase.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,45 +6,99 @@ public class ContractBase {
 	public float perfTimeMult = 1F;
 	public float finalPerf;//how well the contract was completed in the end
 
+	public string contractName;//IMPORTANT: MUST  BE ASSIGNED TO POST POINT DISTRIBUTION
+
 	//stats
 	//0 - 4 corresponds to strength, cleverness, seduction, deception, occult
 	public List<float> statReqList = new List<float>();
 
-	/*
-	 * public float strengthReq;
-	 * public float clevernessReq;
-	 * public float seductionReq;
-	 * public float deceptionReq;
-	 * public float occultReq;
-	*/
+	//sentence is verb + noun + adverb
+	//verb is from top stat, adverb is from 2nd top stat
+	public List<string[]> verbList = new List<string[]>(5);//no need for spaces at the beginning or ends
+	public string[] nounList = {"president", "pizza guy", "tax collector", "pope", "mayor", "internet shitposter"};
+	public List<string[]> adverbList = new List<string[]>(5);
 
 	public ContractBase(float difficulty) {
+		distributeStats (difficulty);
+		initWords ();
+		contractName = writeContractTitle ();
+	}
+	public ContractBase (int[] statReqs, float[] statDifficulties) {//to initialize a contract with a specific set of stat indices with specific difficulties.  the two arrays match up 1 to 1
+		for(int i = 0; i < statReqs.Length; i++) {//for each index i of stat requirements
+			statReqList [statReqs[i]] = statDifficulties [i];//set the stat requirement of the statReq index 
+		}
+		initWords ();
+		contractName = writeContractTitle ();
+	}
+
+	//initializes the arrays of verbs and adverbs
+	private void initWords () {
+		verbList.Add (new string[] {"kill", "hurt", "maim", "crush", "rough up"});//strength
+		verbList.Add (new string[] {"fool", "dupe", "trick", "stupify", "mislead"});//cleverness
+		verbList.Add (new string[] {"attract", "allure", "tempt", "maneuver", "lure"});//seduction
+		verbList.Add (new string[] {"lie to", "swindle", "cheat", "dupe", "delude"});//deception
+		verbList.Add (new string[] {"electrify", "singe", "flood", "reduce", "disintegrate"});//occult
+
+		adverbList.Add (new string[] {"brutally", "painfully", "maliciously", "cruelly", "inhumanely"});
+		adverbList.Add (new string[] {"sneakily", "intelligently", "silently", "dexterously", "adpetly"});
+		adverbList.Add (new string[] {"charmingly", "elegantly", "swankily", "deftly", "nimbly"});
+		adverbList.Add (new string[] {"amorally", "unscrupulously", "unprinciply", "unethically", "slyly"});
+		adverbList.Add (new string[] {"archaically", "arcanely", "magically", "supernaturally", "mystically"});
+	}
+
+	//distributes the difficulty randomly throughout the different stat requirements
+	private void distributeStats (float difficulty) {
 		int statCount = 0;
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < statReqList.Count; i++) {
 			statReqList [i] = Random.value;
 			if(statReqList[i] > 0.5F) {
 				statCount++;
 			}
 		}
 		float diffBase = difficulty / statCount;//the mid value of stat requirements
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < statReqList.Count; i++) {
 			if (statReqList [i] > 0.5F) {
 				statReqList [i] = Random.Range (diffBase - (diffBase / 2), diffBase + (diffBase / 2));
 			} else {
 				statReqList [i] = 0F;
 			}
 		}
-
 	}
-
 
 	//returns a number between 1 and 0 that represents how well a contract went
 	public float getContractPerformance (DemonBase demon) {
-        return 0;
+		float output = 1F;
+		for(int i = 0; i < statReqList.Count; i++) {
+			float f = statReqList [i];
+			if (f > 0.01F) {//avoid div/0 errors
+				output *= demon.statList [i] / statReqList [i];
+			}
+		}
+		finalPerf = output;
+		return output;
 	}
+
+	//returns the time it takes to complete a contract
 	public float getContractCompletionTime (DemonBase demon) {
 		float perf = getContractPerformance (demon);
 		float compTime = perfTimeMult / perf;
-        return 0;
+		return compTime;
 	}
+
+	//builds an appropriate title for the contract
+	public string writeContractTitle () {
+		List<float> orderedStats = statReqList;
+		orderedStats.Sort ();
+		int maxStat = orderedStats.Count - 1;//first and second most important stats
+		int secondMaxStat = orderedStats.Count - 2;
+
+		string noun;
+		string verb = verbList [maxStat] [(int)(Random.value * verbList [maxStat].Length)];
+		string adverb;
+		noun = nounList [(int)(Random.value * nounList.Length)];
+		adverb = adverbList [secondMaxStat] [(int)(Random.value * adverbList [secondMaxStat].Length)];
+
+		return (noun + " the " + verb + " " + adverb);
+	}
+
 }
