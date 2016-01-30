d482a3331d97f051acf721f6bcc8774ef4882272
smoss <Smoss117@gmail.com>
2016-01-30 05:08:31 -0500

    Now randomly assigns things to things for creation of things

---
+++ Assets/Scripts/Ingredients.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ingredients {
+    public enum bodyParts { Head = 2, Limbs = 3, Torso = 5 };
+    public enum stats { Power, Cleverness, Seduction, Deception, Occult };
+    stats stat;
+    string i_name;
+    bodyParts part;
+    public stats GetStat()
+    {
+        return this.stat;
+    }
+    public bodyParts getPart()
+    {
+        return this.part;
+    }
+    public Ingredients(string name, int stat, int part)
+    {
+        this.i_name = name;
+        this.stat = (stats)stat;
+        this.part = (bodyParts)part;
+    }
+    public string ToString()
+    { return this.i_name + " " + this.stat + " " + this.part;
+    }
+}
