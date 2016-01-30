c34511b73f74bf48972324810d1fe58a108ee3f6
Amanda Grice <amandalyngrice@gmail.com>
2016-01-30 00:20:47 -0500

    very basic UI template

---
+++ Assets/PentagramButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PentagramButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DoStuff() {
+		Debug.Log ("pentagram pressed");
+	}
+		
+}
