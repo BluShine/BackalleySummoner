da1b93a2b172955037aaaf922d51d738b4695099
gaben33 <gabenathan98@gmail.com>
2016-01-29 23:50:14 -0500

    Base classes prototype
    Added all the base classes to make the game run.  Assume full working
    functionality for now, still have to write a few methods

---
+++ Assets/Scripts/ContractBase.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ContractBase {
+	public float perfTimeMult = 1F;
+	public float finalPerf;//how well the contract was completed in the end
+
+	//stats
+	//0 - 4 corresponds to strength, cleverness, seduction, deception, occult
+	public List<float> statReqList = new List<float>();
+
+	/*
+	 * public float strengthReq;
+	 * public float clevernessReq;
+	 * public float seductionReq;
+	 * public float deceptionReq;
+	 * public float occultReq;
+	*/
+
+	public ContractBase(float difficulty) {
+		int statCount;
+		for (int i = 0; i < 5; i++) {
+			statReqList [i] = Random.value;
+			if(statReqList[i] > 0.5F) {
+				statCount++;
+			}
+		}
+		float diffBase = difficulty / statCount;//the mid value of stat requirements
+		for (int i = 0; i < 5; i++) {
+			if (statReqList [i] > 0.5F) {
+				statReqList [i] = Random.Range (diffBase - (diffBase / 2), diffBase + (diffBase / 2));
+			} else {
+				statReqList [i] = 0F;
+			}
+		}
+
+	}
+
+
+	//returns a number between 1 and 0 that represents how well a contract went
+	public float getContractPerformance (DemonBase demon) {
+		
+	}
+	public float getContractCompletionTime (DemonBase demon) {
+		float perf = getContractPerformance (demon);
+		float compTime = perfTimeMult / perf;
+	}
+}
---
+++ Assets/Scripts/DemonBase.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DemonBase : MonoBehaviour {
+	public Sprite demonGraphic;
+
+	//demon stats
+	//0 - 4 corresponds to strength, cleverness, seduction, deception, occult
+	public List<float> statList = new List<float>();
+
+	//all items held by the demon
+	public List<ItemBase> itemInventory;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void applyItemBoosts () {
+		foreach (ItemBase i in itemInventory) {
+			for (int j = 0; j < 5; j++) {
+				statList [j] += i.statList [j];
+			}
+		}
+	}
+}
