f9abe0f04ea462a2dcb008e4154c13ccf5ee0f18
Mark <umbreon27@gmail.com>
2016-01-31 12:45:58 -0500

    Working on contract UI
    Contract UI still works kinda.

---
+++ Assets/textHighlighter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class textHighlighter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    Text text;
+    Color startColor;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        startColor = text.color;
+    }
+
+    public void OnPointerEnter(PointerEventData p)
+    {
+        text.color = Color.blue;
+    }
+    public void OnPointerExit(PointerEventData p)
+    {
+        text.color = startColor;
+    }
+}
