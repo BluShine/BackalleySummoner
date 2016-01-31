e5c3a4d3befe04920bdf5a22420fb3a60523aa2f
Courtney Toder <skysyren@gmail.com>
2016-01-31 03:56:50 -0500

    fixing end states

--- Assets/Scripts/EndStates.cs
+++ Assets/Scripts/EndStates.cs
@@ -1,21 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EndStates : MonoBehaviour {
-
 	// Use this for initialization
 	public static void Win(float p)
 	{
+		decimal value = (decimal)p;
+		GameObject.Find("Win").GetComponent<Text>().text = "You got " + value.ToString("C") +" Hellbucks";
 		GameObject.Find ("Win").SetActive (true);
 	}
 
 	public static void Lose(float p)
 	{
+		decimal value = (decimal)p;
+		GameObject.Find("Lose").GetComponent<Text>().text = "You got " + value.ToString("C") +" Hellbucks";
 		GameObject.Find ("Lose").SetActive (true);
 	}
 
 	public static void Neutral(float p)
 	{
+		decimal value = (decimal)p;
+		GameObject.Find("Neutral").GetComponent<Text>().text = "You got " + value.ToString("C") +" Hellbucks";
 		GameObject.Find ("Neutral").SetActive (true);
 	}
 }
