41f3c0f4ca2047d137d169474d410d92463d59fe
Mark <umbreon27@gmail.com>
2016-01-30 22:27:54 -0500

    twitch stuff
    made twitch do things

---
+++ Assets/TwitchReader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwitchReader : MonoBehaviour {
+
+    public TwitchInterface tInterface;
+
+	// Use this for initialization
+	void Start () {
+        //build list of items
+        foreach
+        //add listener
+        tInterface.messageReciever += readMessage;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void readMessage(string message)
+    {
+
+    }
+}
