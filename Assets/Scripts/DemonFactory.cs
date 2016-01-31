88d44ca0846abec62b43f9991f34237d25b260be
Liam <lcheeseboy@gmail.com>
2016-01-30 23:11:36 -0500

    Demon tiers 1-3 done

---
+++ Assets/Scripts/DemonFactory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DemonFactory : MonoBehaviour
+{
+   public static DemonFactory Instance;
+   public GameObject[] DemonTierPrefabs;
+
+   void Awake()
+   {
+      Instance = this;
+      DontDestroyOnLoad(this);
+   }
+
+   public DemonBase makeDemon(Ingredients body, Ingredients limbs, Ingredients horns)
+   {
+      return null;
+   }
+}
