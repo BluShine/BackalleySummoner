b7601558a0cdfb6deaa11f51baeb56a9a35c1ad3
Mark <umbreon27@gmail.com>
2016-01-31 00:47:49 -0500

    Ingredient buttons
    Added ingredient buttons script.

---
+++ Assets/Scripts/IngredientUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class IngredientUI : MonoBehaviour {
+
+    List<Transform> buttonLocations;
+    List<List<GameObject>> buttons;
+
+	// Use this for initialization
+	void Start () {
+        for(int j = 0; j < Recipes.instance.tier_GmO.Count; j++)
+        {
+            int i = 0;
+            GameObject[] g = Recipes.instance.tier_GmO[j].ToArray();
+            buttons.Add(new List<GameObject>());
+            foreach (Transform t in transform)
+            {
+                buttonLocations.Add(t);
+                GameObject newButton = Instantiate(g[i]);
+                newButton.transform.SetParent(buttonLocations[i]);
+                newButton.transform.localPosition = Vector3.zero;
+                buttons[j].Add(newButton);
+                i++;
+            }
+        }
+        OpenTab(0);
+	}
+	
+	public void OpenTab(int tab)
+    {
+        foreach(List<GameObject> l in buttons)
+        {
+            foreach(GameObject g in l)
+            {
+                g.SetActive(false);
+            }
+        }
+        foreach(GameObject b in buttons[tab])
+        {
+            b.SetActive(true);
+        }
+    }
+}
