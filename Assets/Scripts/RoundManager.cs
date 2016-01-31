e2cb55e5915690c64d706bfac811bfc46506f6b9
Courtney Toder <skysyren@gmail.com>
2016-01-31 10:04:53 -0500

    starting end of round screen

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -11,13 +11,27 @@ public class RoundManager : MonoBehaviour {
 	public int roundCount = 0;
 	public GameObject[] disableAfterStart;
 	public void disableInvScreens () {foreach (GameObject g in disableAfterStart) g.SetActive (false);}
+	public GameObject over;
 
 	//assigns 5 contracts, cashes intermediate contracts, gives ingredients
 	public void StartRound () {
+		gameRunner.GenerateContracts();
+		gameRunner.GiveIngredients(0, 8);
+		for(int i = 0; i < contractDisplays.Length; i++) {
+			contractDisplays [i].text = gameRunner.open_contracts [i].contractName;
+		}
+		gameRunner.updateUI ();
+		IngredientUI.UpdateAllNumbers ();
+		roundCount++;
+		over.SetActive (false);
+	}
+
+	public void EndRound()
+	{
 		if (roundCount != 0 && gameRunner.cachedContracts.Count > 0) {
 			gameRunner.cashAllContracts ();
 			foreach (Text t in contractDisplays)
-				t.gameObject.SetActive (false);
+				t.gameObject.SetActive (true);
 		}
 		if (GameManager.instance.reputation < 0) {
 			EndStates.Lose (GameManager.instance.hellBucks);
@@ -34,13 +48,6 @@ public class RoundManager : MonoBehaviour {
 		foreach (ContractBase CB in gameRunner.open_contracts.Where (elem => elem.accepted).ToArray()) {
 			gameRunner.cashContract (CB);
 		}
-		gameRunner.GenerateContracts();
-		gameRunner.GiveIngredients(0, 8);
-		for(int i = 0; i < contractDisplays.Length; i++) {
-			contractDisplays [i].text = gameRunner.open_contracts [i].contractName;
-		}
-		gameRunner.updateUI ();
-		IngredientUI.UpdateAllNumbers ();
-		roundCount++;
+		over.SetActive (true);
 	}
 }
