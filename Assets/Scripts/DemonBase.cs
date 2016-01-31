85f7b7eb7d18abe32c9ac7d6549d2499898c722f
Liam <lcheeseboy@gmail.com>
2016-01-31 14:04:00 -0500

    Demons Wiggle

--- Assets/Scripts/DemonBase.cs
+++ Assets/Scripts/DemonBase.cs
@@ -70,6 +70,17 @@ public class DemonBase : MonoBehaviour {
        {
           ParticleInstance.transform.position = this.transform.position;
        }
+       float[] timePassed = new float[2];
+       timePassed[0] = Random.Range(0, 4 * Mathf.PI);
+       timePassed[1] = Random.Range(0, 4 * Mathf.PI);
+       transform.eulerAngles = new Vector3(0, 0, 10 * Mathf.Sin(timePassed[1]));
+       transform.position += new Vector3(0, 0.005f * Mathf.Sin(timePassed[1] / 2f), 0);
+       int factor = 15;
+       for(int i = 0; i < limbObjects.Length; i++)
+       {
+          limbObjects[i].transform.eulerAngles = new Vector3(0, 0, startRotations[i] + factor * Mathf.Sin(timePassed[0] * ((i < 2) ? 1 : 0.5f)));
+          factor *= -1;
+       }
        transform.localScale = Vector3.zero;
        for(float i = 0; i < 2; i += Time.deltaTime)
        {
@@ -77,16 +88,18 @@ public class DemonBase : MonoBehaviour {
           transform.localScale = new Vector3(ratio, ratio, ratio);
           yield return null;
        }
-       float timePassed = 0;
+
        while(!Input.GetKeyDown(KeyCode.Space))
        {
-          timePassed += 2 * Time.deltaTime;
-          timePassed %= 4 * Mathf.PI;
-          //demon.transform.eulerAngles = new Vector3(0, 0, 20 * Mathf.Sin(timePassed));
-          int factor = 15;
+          timePassed[0] += 2 * Time.deltaTime;
+          timePassed[0] %= 4 * Mathf.PI;
+          timePassed[1] += 1.5f * Time.deltaTime;
+          timePassed[1] %= 4 * Mathf.PI;
+          transform.eulerAngles = new Vector3(0, 0, 10 * Mathf.Sin(timePassed[1]));
+          transform.position += new Vector3(0, 0.005f * Mathf.Sin(timePassed[1] / 2f), 0);
           for(int i = 0; i < limbObjects.Length; i++)
           {
-             limbObjects[i].transform.eulerAngles = new Vector3(0, 0, startRotations[i] + factor * Mathf.Sin(timePassed * ((i < 2) ? 1 : 0.5f)));
+             limbObjects[i].transform.eulerAngles = new Vector3(0, 0, startRotations[i] + factor * Mathf.Sin(timePassed[0] * ((i < 2) ? 1 : 0.5f)));
              factor *= -1;
           }
           yield return null;
