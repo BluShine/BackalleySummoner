36e36bd81acbf0c0c1be6788c4fab1df8029aadb
Courtney Toder <skysyren@gmail.com>
2016-01-31 03:15:38 -0500

    changes to round manager

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -8,13 +8,26 @@ public class RoundManager : MonoBehaviour {
 	public GameManager gameRunner;
 	public List<ContractBase> completedContracts;
 	public Text[] contractDisplays = new Text[6];
+	public int roundCount;
 	void Start () {
+		this.roundCount = 1;
 		//gameRunner.GenerateContracts ();
 		//StartRound ();
 	}
 
 	//assigns 5 contracts, cashes intermediate contracts, gives ingredients
 	public void StartRound () {
+		if (GameManager.instance.reputation < 0) {
+			GameObject.Find ("lose-screen").SetActive (true);
+			return;
+		} elif(this.roundCount > 10 && GameManager.instance.hellBucks > 200)
+		{
+			GameObject.Find("win-screen").SetActive(true);
+			return;
+		} elif(this.roundCount > 10)
+		{
+			GameObject.Find("end-screen").SetActive(true);
+		}
 		foreach (ContractBase CB in gameRunner.open_contracts.Where (elem => elem.accepted).ToArray()) {
 			gameRunner.cashContract (CB);
 		}
